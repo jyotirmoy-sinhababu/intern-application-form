5ae6c2e5a807756a8354e9f3a5f8aff087faa12f
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-01-26 00:18:25 +0530

    save btn created, onchange function, increment and decrement function created

--- src/component/display/Display.js
+++ src/component/display/Display.js
@@ -6,22 +6,33 @@ import { useState } from 'react';
 
 const Display = ({ data }) => {
   const [count, setCount] = useState(0);
+  const [inputData, setInputData] = useState({});
 
   const incrementCount = () => {
-    if (count >= 0) setCount(count + 1);
+    setCount((prev) => prev + 1);
   };
 
   const decrementCount = () => {
     if (count != 0) setCount(count - 1);
   };
 
+  const handleChange = (e) => {
+    setInputData({ ...inputData, [e.target.name]: e.target.value });
+  };
+
+  console.log(inputData);
+
   return (
     <>
       <p>{data ? data[count]?.question_text : null}</p>
-      <InputField data={data} count={count} />
+      <InputField data={data} count={count} handleChange={handleChange} />
       <div>
         <button onClick={decrementCount}>prev</button>
-        <button onClick={incrementCount}>next</button>
+        {count > 9 ? (
+          <button>Save</button>
+        ) : (
+          <button onClick={incrementCount}>next</button>
+        )}
       </div>
     </>
   );
--- src/component/input/InputField.js
+++ src/component/input/InputField.js
@@ -1,43 +1,64 @@
 import React from 'react';
 
-const InputField = ({ data, count }) => {
-  console.log(count);
+const InputField = ({ data, count, handleChange }) => {
   if (data) {
     if (data[count]?.question_type == 4) {
       return (
         <>
           <form>
-            <input type='text' className='input-field' />
+            <input
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              type='text'
+              className='input-field'
+              name={data[count].question_text}
+            />
           </form>
         </>
       );
-      if (data[count]?.question_type == 1 || 2) {
-        return (
-          <>
-            <form>
-              {data[count]?.choices.map((item) => {
-                return (
-                  <>
-                    <form key={item.id}>
-                      <input type='checkbox' className='checkbox' />
-                      <label htmlFor='option'>{item.choice_text}</label>
-                    </form>
-                  </>
-                );
-              })}
-            </form>
-          </>
-        );
-      }
-      if (data[count]?.question_type == 5) {
-        return (
-          <>
-            <form>
-              <input type='file' className='file-input' />
-            </form>
-          </>
-        );
-      }
+    } else if (
+      data[count]?.question_type == 1 ||
+      data[count]?.question_type == 2 ||
+      data[count]?.question_type == 2
+    ) {
+      return (
+        <>
+          {data[count]?.choices?.map((item) => {
+            return (
+              <>
+                <form key={item.id}>
+                  <input
+                    onChange={(e) => {
+                      handleChange(e);
+                    }}
+                    type='checkbox'
+                    className='checkbox'
+                    name={item.choice_text}
+                  />
+                  <label htmlFor={item.choice_text}>{item.choice_text}</label>
+                </form>
+              </>
+            );
+          })}
+        </>
+      );
+    } else if (data[count]?.question_type == 5) {
+      return (
+        <>
+          <form>
+            <label for='myfile'>Select a file:</label>
+            <input
+              onChange={(e) => {
+                handleChange(e);
+              }}
+              type='file'
+              className='file-input'
+              name={data.choice_text}
+            />
+          </form>
+        </>
+      );
     }
   }
 };
