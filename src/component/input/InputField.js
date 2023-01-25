3162fd7a6d7cc312bdd70305912849cafd897af3
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-01-25 12:10:40 +0530

    input method for different question type

--- src/component/input/InputField.js
+++ src/component/input/InputField.js
@@ -1,7 +1,44 @@
 import React from 'react';
 
 const InputField = ({ data, count }) => {
-  return <div></div>;
+  if (data) {
+    if (data[count]?.question_type == 4) {
+      return (
+        <>
+          <form>
+            <input type='text' className='input-field' />
+          </form>
+        </>
+      );
+      if (data[count]?.question_type == 1 || 2) {
+        return (
+          <>
+            <form>
+              {data[count]?.choices.map((item) => {
+                return (
+                  <>
+                    <form key={item.id}>
+                      <input type='checkbox' className='checkbox' />
+                      <label htmlFor='option'>{item.choice_text}</label>
+                    </form>
+                  </>
+                );
+              })}
+            </form>
+          </>
+        );
+      }
+      if (data[count]?.question_type == 5) {
+        return (
+          <>
+            <form>
+              <input type='file' className='file-input' />
+            </form>
+          </>
+        );
+      }
+    }
+  }
 };
 
 export default InputField;
