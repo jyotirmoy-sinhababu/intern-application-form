e6684db22f9e0b08786c8918bd860a1975f66adb
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-01-27 10:32:41 +0530

    style added to nav

--- src/component/input/InputField.js
+++ src/component/input/InputField.js
@@ -11,8 +11,10 @@ const InputField = ({ data, count }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    localStorage.setItem('inputData', JSON.stringify(inputData));
   };
 
+  console.log(inputData);
   if (data) {
     return (
       <form
@@ -23,8 +25,12 @@ const InputField = ({ data, count }) => {
         {data[count].question_type == 4 ? (
           <input
             type='text'
-            name={data[count].question_type}
+            name={data[count].question_text}
+            required
             className='input-field'
+            onChange={(e) => {
+              handleChange(e);
+            }}
           />
         ) : data[count].question_type == 1 ||
           data[count].question_type == 2 ||
@@ -35,8 +41,12 @@ const InputField = ({ data, count }) => {
                 <div key={item.id}>
                   <input
                     type='checkbox'
+                    required
                     name={item.choice_text}
                     className='input-checkbox'
+                    onChange={(e) => {
+                      handleChange(e);
+                    }}
                   />
                   <label>{item.choice_text}</label>
                 </div>
@@ -44,7 +54,14 @@ const InputField = ({ data, count }) => {
             })}
           </div>
         ) : (data[count].question_type = 5) ? (
-          <input type='file' name={data[count].question_type} />
+          <input
+            type='file'
+            required
+            name={data[count].question_type}
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          />
         ) : (
           <p>Loading...</p>
         )}
--- src/component/nav/Nav.js
+++ src/component/nav/Nav.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './nav.css';
+
 const Nav = ({ data }) => {
   return (
-    <div>
-      <p>{data?.setting?.title}</p>
+    <div className='nav-cnt'>
+      <p className='nav-heading'>{data?.setting?.title}</p>
     </div>
   );
 };
